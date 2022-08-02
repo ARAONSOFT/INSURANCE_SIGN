e5c4741de5cea687a0e83f0d5b6b194a53193740
NewChamchi <51375290+NewChamchi@users.noreply.github.com>
2022-08-02 14:18:23 +0900

    보험 계약

--- src/components/ButtonsHeaderBar.js
+++ src/components/ButtonsHeaderBar.js
@@ -4,29 +4,29 @@ import ButtonToBeforePage from './ButtonToBeforePage';
 import ButtonToEntirePage from './ButtonToEntirePage';
 
 const ButtonsHeaderBar = ({
-  pageIndex,
-  setPageIndex,
-  isEntirePage,
-  setIsEntirePage,
+	pageIndex,
+	setPageIndex,
+	isEntirePage,
+	setIsEntirePage,
 }) => {
-  return (
-    <div className="ButtonsBar ButtonsHeaderBar">
-      <ButtonToBeforePage
-        pageIndex={pageIndex}
-        setPageIndex={setPageIndex}
-        isEntirePage={isEntirePage}
-      />
-      <ButtonToEntirePage
-        isEntirePage={isEntirePage}
-        setIsEntirePage={setIsEntirePage}
-      />
-      <ButtonToAfterPage
-        pageIndex={pageIndex}
-        setPageIndex={setPageIndex}
-        isEntirePage={isEntirePage}
-      />
-    </div>
-  );
+	return (
+		<div className="ButtonsBar ButtonsHeaderBar">
+			<ButtonToBeforePage
+				pageIndex={pageIndex}
+				setPageIndex={setPageIndex}
+				isEntirePage={isEntirePage}
+			/>
+			<ButtonToEntirePage
+				isEntirePage={isEntirePage}
+				setIsEntirePage={setIsEntirePage}
+			/>
+			<ButtonToAfterPage
+				pageIndex={pageIndex}
+				setPageIndex={setPageIndex}
+				isEntirePage={isEntirePage}
+			/>
+		</div>
+	);
 };
 
 export default ButtonsHeaderBar;
